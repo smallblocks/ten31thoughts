1069f54aa49f91c7ea196c8566177af89a814f99
smallblocks <smallblocks@users.noreply.github.com>
2026-03-15 03:59:14 +0000

    fix: correct index.ts exports to match SDK pattern

--- startos/index.ts
+++ startos/index.ts
@@ -1,4 +1,11 @@
-export { manifest } from './manifest'
-export { init, uninit } from './init'
+/**
+ * Ten31 Thoughts - StartOS SDK Entry Point
+ */
+export { createBackup } from './backups'
 export { main } from './main'
-export { backups } from './backups'
+export { init, uninit } from './init'
+export { actions } from './actions'
+import { buildManifest } from '@start9labs/start-sdk'
+import { manifest as sdkManifest } from './manifest'
+import { versionGraph } from './install/versionGraph'
+export const manifest = buildManifest(versionGraph, sdkManifest)
